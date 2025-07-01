7c0b5a3ed23cf6d21dbbfc93220db7d37a370277
22AK1A0410 <bhavanipamaluru@gmail.com>
2025-07-01 12:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from ibm_watsonx_ai.foundation_models import ModelInference
-
 # Watsonx credentials and settings
 model_id = "ibm/granite-3-8b-instruct"
 project_id = "ef457d57-bdd1-49aa-9e80-53bd5b3afbe8" 
@@ -8,12 +7,9 @@ credentials = {
     "url": "https://eu-de.ml.cloud.ibm.com", 
     "apikey": "S1zl_mgeQVqjItIpyy8d6-ZQmA0oP82sJ0x-d5AiPhpk"  
 }
-
 # Streamlit UI
 st.title("EduTutor AI")
-
 question = st.text_input("Ask your question:")
-
 if st.button("Get Answer") and question.strip() != "":
     model = ModelInference(
         model_id=model_id,
